867f877163933317871d5a533ec646be866ea03a
Adam Kovar <akovar@iu.edu>
2025-08-29 19:58:09 +0000

    Prepping Assignments for submission

--- apputil.py
+++ apputil.py
@@ -1,3 +1,14 @@
 
 
 # add code below ...
+
+def palindrome(s):
+    cleaned = ''.join(c.lower() for c in s if c.isalnum())
+    # Check if the cleaned string is equal to its reverse
+    return cleaned == cleaned[::-1]
+
+
+
+
+def parentheses(s):
+    return s.count('(') == s.count(')')
